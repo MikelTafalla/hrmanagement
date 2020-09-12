358d0bfc1988029bd8d0b06d6428dd33f5337e12
Pablo Vazquez <pvazquezems@gmail.com>
2020-09-12 13:19:57 -0700

    fixed section c styling

--- client/src/components/sectionC/SectionC.js
+++ client/src/components/sectionC/SectionC.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Agreement from "./agreement.json"
-
+import Agreement from "./agreement.json";
 
 function SectionC(props) {
   const [activeAgreement, setActiveAgreement] = useState([{}]);
@@ -12,254 +11,179 @@ function SectionC(props) {
       if (cities[i] === "Adelaide") {
         return setActiveAgreement(Agreement[i]);
       }
-    } 
+    }
   }, []);
 
   return (
     <div>
-      <div className="ui form" style={{ margin: "100px" }}>
-        <div className="inline fields">
-          <div className="fourteen wide field" style={{ backgroundColor: "teal" }}>
-            <label>
-              <h3 style={{ color: "white", padding: "10px" }}>
-                {props.sectionC}
-              </h3>
-            </label>
-          </div>
+      <div className="ui padded grid container">
+        <div className="sixteen wide fluid centered teal column">
+          {props.sectionC}
         </div>
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Employment Agreement:</label>
+
+        <div className="row">
+          <div className="five wide column">Employment Agreement:</div>
+          <div className="row">
+            <div className="five wide column">Current</div>
+            <select className="ten wide column border">
+              {/* {activeAgreement.map(agr => (
+        <option key={agr.key} value={`${agr.value}`}>{agr.name}</option>
+      ))} */}
+            </select>
           </div>
-          <div className="six wide field" style={{ textAlign: "center" }}>
-            <div className="field">
-              <label>Current</label>
-              <select>
-                {/* {activeAgreement.map(agr => (
-                  <option key={agr.key} value={`${agr.value}`}>{agr.name}</option>
-                ))} */}
-              </select>
-            </div>
-          </div>
-          <div className="six wide field" style={{ textAlign: "center" }}>
-            <div className="field">
-              <label>Proposed</label>
-              <select>
-                <option value="">option one</option>
-                <option value="">option two</option>
-                <option value="">option three</option>
-                <option value="">option four</option>
-              </select>
-            </div>
+          <div className="one wide column"></div>
+          <div className="row">
+            <div className="five wide column">Proposed</div>
+            <select className="ten wide column border">
+              <option value="">option one</option>
+              <option value="">option two</option>
+              <option value="">option three</option>
+              <option value="">option four</option>
+            </select>
           </div>
         </div>
 
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Classification:</label>
+        <div className="row">
+          <div className="five wide column">Classification:</div>
+          <div className="row">
+            <div className="five wide column">Unknown Field</div>
+            <select>
+              <option value="">option one</option>
+              <option value="">option two</option>
+              <option value="">option three</option>
+              <option value="">option four</option>
+            </select>
           </div>
-          <div className="six wide field" style={{ textAlign: "center" }}>
-            <div className="field">
-              <label>Unk field</label>
-              <select>
-                <option value="">option one</option>
-                <option value="">option two</option>
-                <option value="">option three</option>
-                <option value="">option four</option>
-              </select>
-            </div>
-          </div>
-          <div className="six wide field" style={{ textAlign: "center" }}>
-            <div className="field">
-              <label>Unk field</label>
-              <select>
-                <option value="">option one</option>
-                <option value="">option two</option>
-                <option value="">option three</option>
-                <option value="">option four</option>
-              </select>
-            </div>
+          <div className="one wide column"></div>
+          <div className="row">
+            <div className="five wide column">Unknown Field 2</div>
+            <select className="ten wide column border">
+              <option value="">option one</option>
+              <option value="">option two</option>
+              <option value="">option three</option>
+              <option value="">option four</option>
+            </select>
           </div>
         </div>
 
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Team Leader Allowance:</label>
-          </div>
-          <div className="ten wide field">
-            <div className="field">
-              <input type="text" placeholder="Leader Allowance" defaultValue={props.team_leader_allowance}></input>
-            </div>
+        <div className="row">
+          <div className="five wide column">Team Leader Allowance:</div>
+          <input
+            type="text"
+            placeholder="Team Leader Allowance"
+            defaultValue={props.team_leader_allowance}
+          ></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">Leading Hand Allowance:</div>
+          <input
+            type="text"
+            placeholder="Leading Hand Allowance"
+            defaultValue={props.leading_hand_allowance}
+          ></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">Dual Trade Allowance:</div>
+          <input
+            type="text"
+            placeholder="Dual Trade Allowance"
+            defaultValue={props.dual_trade_allowance}
+          ></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">Other Allowances:</div>
+          <input
+            type="text"
+            placeholder="Dual Trade Allowance"
+            defaultValue={props.other_allowance}
+          ></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">Work Schedule:</div>
+          <div className="five wide column">
+            <select>
+              <option value="">Day</option>
+              <option value="">Shift</option>
+            </select>
           </div>
         </div>
 
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Leading Hand Allowance:</label>
-          </div>
-          <div className="ten wide field">
-            <div className="field">
-              <input type="text" placeholder="Leading hand Allowance" defaultValue={props.leading_hand_allowance}></input>
-            </div>
-          </div>
-        </div>
-
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Dual Trade Allowance:</label>
-          </div>
-          <div className="ten wide field">
-            <div className="field">
-              <input type="text" placeholder="Trade Allowance" defaultValue={props.dual_trade_allowance}></input>
-            </div>
+        <div className="row">
+          <div className="five wide column">Shift:</div>
+          <div className="five wide column">
+            <select>
+              <option value="">N/A</option>
+              <option value="">Shift-A</option>
+              <option value="">Shift-B</option>
+              <option value="">Shift-C</option>
+              <option value="">Shift-D</option>
+              <option value="">Shift-E</option>
+              <option value="">Shift-1</option>
+              <option value="">Shift-2</option>
+              <option value="">Shift-3</option>
+              <option value="">Shift-4</option>
+              <option value="">Shift-5</option>
+            </select>
           </div>
         </div>
 
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Other Allowances (please specify):</label>
+        <div className="row">
+          <div className="five wide column">
+            <h3>Work Days (Waged Employees)</h3>Tick the check box for the new
+            work days
           </div>
-          <div className="ten wide field">
-            <div className="field">
-              <input type="text" placeholder="Other Allowances" defaultValue={props.other_allowances}></input>
-            </div>
-          </div>
-        </div>
-
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Work Schedule:</label>
-          </div>
-          <div className="ten wide field">
-            <div className="field">
-              <select>
-                <option value="">Day</option>
-                <option value="">Shift</option>
-              </select>
-            </div>
+          <div className="five wide column">
+            <h3>Hours of Work</h3>Fill-in the work per day
           </div>
         </div>
 
-        <div className="inline fields">
-          <div className="six wide field">
-            <label>Shift:</label>
-          </div>
-          <div className="ten wide field">
-            <div className="field">
-              <select>
-                <option value="">N/A</option>
-                <option value="">Shift-A</option>
-                <option value="">Shift-B</option>
-                <option value="">Shift-C</option>
-                <option value="">Shift-D</option>
-                <option value="">Shift-E</option>
-                <option value="">Shift-1</option>
-                <option value="">Shift-2</option>
-                <option value="">Shift-3</option>
-                <option value="">Shift-4</option>
-                <option value="">Shift-5</option>
-              </select>
-            </div>
-          </div>
+        <div className="row">
+          <div className="five wide column">Monday</div>
+          <input className="five wide column border"></input>
         </div>
 
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>
-              <h3>Work Days (waged employee):</h3>
-            </label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <label>
-                <h3>Hours Of Work:</h3>
-              </label>
-            </div>
-          </div>
+        <div className="row">
+          <div className="five wide column">Tuesday</div>
+          <input className="five wide column border"></input>
         </div>
 
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>Monday:</label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Monday"></input>
-            </div>
-          </div>
-        </div>
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>Tuesday:</label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Tuesday"></input>
-            </div>
-          </div>
-        </div>
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>Wednesday:</label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Wednesday"></input>
-            </div>
-          </div>
-        </div>
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>Thursday:</label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Thursday"></input>
-            </div>
-          </div>
-        </div>
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>Friday:</label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Friday"></input>
-            </div>
-          </div>
-        </div>
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>Saturday:</label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Saturday"></input>
-            </div>
-          </div>
-        </div>
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>Sunday:</label>
-          </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Sunday"></input>
-            </div>
-          </div>
+        <div className="row">
+          <div className="five wide column">Wednesday</div>
+          <input className="five wide column border"></input>
         </div>
 
-        <div className="inline fields">
-          <div className="eight wide field">
-            <label>
-              <h3>TOTAL HOURS:</h3>
-            </label>
+        <div className="row">
+          <div className="five wide column">Thursday</div>
+          <input className="five wide column border"></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">Friday</div>
+          <input className="five wide column border"></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">Saturday</div>
+          <input className="five wide column border"></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">Sunday</div>
+          <input className="five wide column border"></input>
+        </div>
+
+        <div className="row">
+          <div className="five wide column">
+            <h3>Total Hours Per Week</h3>
           </div>
-          <div className="eight wide field">
-            <div className="field">
-              <input type="text" placeholder="Total Hours" defaultValue={props.total_hours_C}></input>
-            </div>
-          </div>
+          <input
+            className="two wide column border"
+            defaultValue={props.total_hours_C}
+          ></input>
         </div>
       </div>
     </div>
