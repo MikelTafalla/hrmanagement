1904a84520d5c5779b0fd7a29ea92395a3771808
Shaleem Malik <shaleemmalik6@gmail.com>
2020-09-12 16:29:46 -0400

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./App.css";
-//import EmployeeChangeForm from "./page/employeeChangeForm/EmployeeChangeForm";
-//import DepartmentValidationForm from "./page/departmentValidationForm/DepartmentValidationForm";
-//import FormListPage from "./page/formListPage/FormListPage";
+import EmployeeChangeForm from "./page/employeeChangeForm/EmployeeChangeForm";
+import DepartmentValidationForm from "./page/departmentValidationForm/DepartmentValidationForm";
+import FormListPage from "./page/formListPage/FormListPage";
 import EmployeeTable from "./page/employeeTable/EmployeeTable";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+      <Switch>
     <div>
-      {/* <DepartmentValidationForm /> */}
-      {/*<FormListPage/> */}
-      {/*<EmployeeChangeForm />*/}
-      <EmployeeTable />
+     <Route path="/" exact component={DepartmentValidationForm} />
+     <Route path="/employeetable" component={EmployeeTable} />
+     <Route path="/formlistpage" component={FormListPage} />
+     <Route path="/employeechangeForm" component={EmployeeChangeForm} />
     </div>
+    </Switch>
+    </Router>
   );
 }
 
--- client/src/components/role/Role.js
+++ client/src/components/role/Role.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 import "./Role.css"
+import { Link } from 'react-router-dom'
 
 const CardColored = () => {
     const cardClick = (selectedCard) => {
@@ -11,17 +12,11 @@ const CardColored = () => {
         
 
         <Card.Group centered stackable itemsPerRow={3}>
-            <div><Card name='manager' color='blue' image={'./images/manager.png'} /><button className='ui violet inverted fluid button large' onClick={(e) => cardClick(e.target.value)} value='manager'>Management Department</button></div>
-        
-            <div><Card name='humanResource' color='violet' image={'./images/hrimage4.png'} /><button className='ui violet inverted fluid button large' onClick={(e) => cardClick(e.target.value)} value='humanResource'>HR Department</button></div>
-            <div><Card name='payroll' color='green' image={'./images/payroll.png'} /><button className='ui violet inverted fluid button large' onClick={(e) => cardClick(e.target.value)} value='payroll'>Payroll Department</button></div>
+            <Link to='employeetable'><div><Card name='manager' color='blue' image={'./images/manager.png'} /><button className='ui violet inverted fluid button large' onClick={(e) => cardClick(e.target.value)} value='manager'>Management Department</button></div></Link>
+            <Link to='employeetable'><div><Card name='humanResource' color='violet' image={'./images/hrimage4.png'} /><button className='ui violet inverted fluid button large' onClick={(e) => cardClick(e.target.value)} value='humanResource'>HR Department</button></div></Link>
+            <Link to='employeetable'><div><Card name='payroll' color='green' image={'./images/payroll.png'} /><button className='ui violet inverted fluid button large' onClick={(e) => cardClick(e.target.value)} value='payroll'>Payroll Department</button></div></Link>
         </Card.Group>
 
-//         <Container>
-
-// <Image src='hrmanagement/client/public/images/manager.png' size='medium' />
-            
-//             </Container>
     )
 
 }
