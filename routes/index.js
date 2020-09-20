edbc50592f291ae5ff2f059372e61f3fe33d6cc2
Shaleem Malik <shaleemmalik6@gmail.com>
2020-09-19 22:52:07 -0400

    testing deployment

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const employeeRoutes = require("./api/employeeRoutes");
 router.use("/api", employeeRoutes);
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build"));
 });
 module.exports = router;
 
